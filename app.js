25cfc0e12fcae2c29067e47a568783b92e9d41c1
Sayan.Divakaran <sayan.divakaran@gmail.com>
2023-01-25 14:10:02 -0500

    inital load

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var randomWords = require('random-words');
+const memer = require("random-jokes-api");
+
+var app = express();
+
+app.get("/jokes", (req, res, next) => {
+    let jokes = memer.joke();
+
+    res.json(jokes);
+});
+
+
+app.get("/generate", (req, res, next) => {
+    var rword = randomWords();
+
+    res.json(rword);
+});
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
